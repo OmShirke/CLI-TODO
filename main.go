18cb78035d811fbf42c26df74e66ff9237e0bfa2
OmShirke <113794365+OmShirke@users.noreply.github.com>
2024-10-30 03:00:44 +0530

    added storage and command to run in CLI, Projected Completed

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 func main() {
 	todos := Todos{}
-	todos.add("buy milk")
-	todos.add("buy bread")
-	todos.add("buy paper")
-	todos.add("do homework")
-	todos.toggle(0)
-	todos.toggle(2)
-	todos.print()
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
+	storage.Save(todos)
 }
